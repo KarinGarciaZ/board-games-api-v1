a56badaa9b9268ec6a3c86620c7f4ae28b3b22da
Miriam Villa <miriam.villa@classmates.com>
2024-04-17 20:27:32 -0600

    add family endpoint

--- api/families/families.service.js
+++ api/families/families.service.js
@@ -2,8 +2,8 @@ const { connection } = require('../../sql/connection-sql');
 
 const getFamilies = async () => {
   try {
-    const resp = await connection.query(`SELECT * FROM families`);
-    return resp;
+    const [row] = await connection.query(`SELECT * FROM families`);
+    return row;
   } catch (error) {
     throw error;
   }
@@ -11,14 +11,25 @@ const getFamilies = async () => {
 
 const getFamily = async (id) => {
   try {
-    const [row] = await connection.query(`SELECT * FROM families WHERE id = ${id}`);
+    const [row] = await connection.query(`SELECT * FROM families WHERE id = ?`, [id]);
     return row;
   } catch (error) {
     throw error;
   }
 };
 
+const addFamily = async (body) => {
+  try {
+    const [row] = await connection.query(`INSERT INTO families SET ?`, [body]);
+    console.log(row)
+    return row;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   getFamilies,
-  getFamily
+  getFamily,
+  addFamily
 }
