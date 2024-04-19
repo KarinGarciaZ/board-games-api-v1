f856832bde16615f743b98554b01a3724d241d78
Miriam Villa <miriam.villa@classmates.com>
2024-04-19 16:58:23 -0600

    mock transaction

--- seeds/index.js
+++ seeds/index.js
@@ -4,23 +4,37 @@ const { familiesMock } = require('./families.mock');
 const { gamesMock } = require('./games.mock');
 
 const seedDatabase = async () => {
+  const newConnection = await connection.getConnection();
   try {
-
+    await newConnection.beginTransaction();
       // Insert seed data into the families table
       for (const family of familiesMock) {
-          await connection.query(
-            'INSERT INTO families (name, deleted) values (?, ?)',
-            [family.name, family.deleted]
+          await newConnection.query(
+            'INSERT INTO families SET ?',
+            [family]
           );
       }
 
-      // Close the database connection
-      await connection.end();
+      for (const brand of brandsMock) {
+        await newConnection.query(
+          'INSERT INTO brands SET ?', [brand]
+        );
+      }
+
+      for (const game of gamesMock) {
+        await newConnection.query(
+          'INSERT INTO games SET ?', [game]
+        );
+      }
+      await newConnection.commit();
 
       console.log('Seed data inserted successfully!');
   } catch (error) {
+      await newConnection.rollback();
       console.error('Error seeding database:', error);
   }
+  // Close the database connection
+  await newConnection.end();
 }
 
 // Call the function to seed the database
