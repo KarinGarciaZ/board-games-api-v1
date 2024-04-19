a5a9d580a042ee1abd74a40a56de17a45444daa3
Miriam Villa <miriam.villa@classmates.com>
2024-04-18 18:51:03 -0600

    brandsMock

--- seeds/brands.mock.js
+++ seeds/brands.mock.js
@@ -1,4 +1,15 @@
-const brandsMock = [];
+const brandsMock = [
+  { id: null, name: 'Goliath', deleted: false },
+  { id: null, name: 'Asmodee', deleted: false },
+  { id: null, name: 'Hasbro', deleted: false },
+  { id: null, name: 'Unstable games', deleted: false },
+  { id: null, name: 'Catan studio', deleted: false },
+  { id: null, name: 'Wild card games', deleted: false },
+  { id: null, name: 'Days of wonder', deleted: false },
+  { id: null, name: 'The oatmeal', deleted: false },
+  { id: null, name: 'Mattel', deleted: false },
+  { id: null, name: 'Blue Orange', deleted: false },
+];
 
 module.exports = {
   brandsMock
