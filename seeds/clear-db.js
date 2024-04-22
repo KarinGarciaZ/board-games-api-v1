50be3bfb61d21b60c2cbf6acd68c93fbb2c5d648
Oscar Garcia <oscarjovannydeveloper@gmail.com>
2024-04-22 17:59:52 -0600

    add clear db script

---
+++ seeds/clear-db.js
@@ -0,0 +1,24 @@
+const { connection } = require('../sql/connection-sql');
+
+const seedDatabase = async () => {
+  const newConnection = await connection.getConnection();
+  try {
+    await newConnection.beginTransaction();
+    await newConnection.query('TRUNCATE TABLE families');
+    await newConnection.query('TRUNCATE TABLE brands');
+    await newConnection.query('TRUNCATE TABLE games');
+    await newConnection.query('TRUNCATE TABLE versions');
+
+    await newConnection.commit();
+
+    console.log('Data deleted successfully!');
+  } catch (error) {
+      await newConnection.rollback();
+      console.error('Error deleting tables into database:', error);
+  }
+  // Close the database connection
+  await newConnection.end();
+}
+
+// Call the function to seed the database
+seedDatabase();
