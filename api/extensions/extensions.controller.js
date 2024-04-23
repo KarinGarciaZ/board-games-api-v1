3bec76030b8ab1bb9c2452562f8c814462c2c6fb
Miriam Villa <miriam.villa@classmates.com>
2024-04-23 17:10:16 -0600

    extensions endpoints

--- api/extensions/extensions.controller.js
+++ api/extensions/extensions.controller.js
@@ -1,21 +1,48 @@
 const express = require('express');
+const ExtensionsService = require('./extensions.service');
 
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-  res.status(200).send();
+  const id = req.params.id;
+  try {
+    const extension = await ExtensionsService.getExtension(id);
+    res.status(200).json(extension);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', async (req, res) => {
-  res.status(201).send();
+  const body = req.body;
+  body.id = null;
+  try {
+    await ExtensionsService.addExtension(body)
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  res.status(201).send();
+  const id = req.params.id;
+  const {id: bodyId, ...body} = req.body
+  try {
+    await ExtensionsService.updateExtension(id, body);
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  res.status(200).send();
+  const id = req.params.id;
+  try {
+    ExtensionsService.deleteExtension(id);
+    res.status(200).send();
+  } catch (error) {
+    res.status(200).json(error);
+  }
 });
 
 module.exports = {
--- api/extensions/extensions.service.js
+++ api/extensions/extensions.service.js
@@ -7,8 +7,51 @@ const getExtensionsByGameId = async (id) => {
   } catch (error) {
     throw error;
   }
+};
+
+const getExtension = async (id) => {
+  try {
+    const [rows] = await connection.query(`SELECT * FROM extensions WHERE id = ? AND deleted = false`, [id]);
+    if(rows.length) {
+      return rows[0];
+    }
+    return {};
+  } catch (error) {
+    throw error;
+  }
+};
+
+const addExtension = async (body) => {
+  try {
+    await connection.query(`INSERT INTO extensions SET ?`, [body]);
+    return;
+  } catch (error) {
+    throw error
+  }
+};
+
+const updateExtension = async (id, body) => {
+  try {
+    await connection.query(`UPDATE extensions SET ? WHERE id = ?`, [body, id]);
+    return;
+  } catch (error){
+    throw error
+  }
+};
+
+const deleteExtension = async (id) => {
+  try {
+    await connection.query(`UPDATE extensions SET deleted = true WHERE ID = ?`, [id]);
+    return;
+  } catch (error) {
+    throw (error)
+  }
 }
 
 module.exports = {
-  getExtensionsByGameId
+  getExtensionsByGameId,
+  getExtension,
+  addExtension,
+  updateExtension,
+  deleteExtension
 }
