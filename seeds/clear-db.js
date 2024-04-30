7102eeb80db6a191cf5f75d5c122c31562fec3b5
Oscar Garcia <oscarjovannydeveloper@gmail.com>
2024-04-30 17:30:17 -0600

    add truncate seeds

--- seeds/clear-db.js
+++ seeds/clear-db.js
@@ -1,17 +1,20 @@
 const { sequelize } = require('../sql/sequelize-connection');
 const Family = require('../sql/models/family');
+const Brand = require('../sql/models/brand');
+const Game = require('../sql/models/game');
+const Version = require('../sql/models/version');
+const Extension = require('../sql/models/extension');
 
 const seedDatabase = async () => {
   const t = await sequelize.transaction();
   try {
     await Family.destroy({ truncate: { cascade: true }, transaction: t });
-    // await newConnection.query('TRUNCATE TABLE brands');
-    // await newConnection.query('TRUNCATE TABLE games');
-    // await newConnection.query('TRUNCATE TABLE versions');
-    // await newConnection.query('TRUNCATE TABLE extensions');
+    await Brand.destroy({ truncate: { cascade: true }, transaction: t });
+    await Game.destroy({ truncate: { cascade: true }, transaction: t });
+    await Version.destroy({ truncate: { cascade: true }, transaction: t });
+    await Extension.destroy({ truncate: { cascade: true }, transaction: t });
 
     await t.commit();
-
     console.log('Data deleted successfully!');
   } catch (error) {
     await t.rollback();
